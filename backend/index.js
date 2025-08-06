51b1656aa3ee15d78dca05ab269e11db3f545f68
bhargav82 <bhargav0831@gmail.com>
2025-08-06 08:50:00 -0500

    fixed bug, now shows table and chart, added env and gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 // External Libraries
+import { GoogleGenerativeAI } from '@google/generative-ai'
 const express = require('express');
 const cors = require('cors');
 const SerialPort = require('serialport');
-const ReadLine = require('readline');
+const { ReadlineParser } = require('@serialport/parser-readline');
+require('dotenv').config();
 
-// Set up Express server
+
+// Setup Express server
 const app = express();
 const port = 3001;
 
@@ -17,18 +20,35 @@ app.use(express.json());
 let emgData = [];
 let recording = false;
 
+const ai = new GoogleGenerativeAI({});
+const apiKey = process.env.apiKey;
 
-// SerialPort setup - to read in values (until a new line) from Arduino through port
+
+// SerialPort setup - to read in values (until a new line) from Arduino microcontroller through port
 const serial = new SerialPort.SerialPort({path: '/dev/tty.usbserial-110', baudRate: 115200});
-const parser = new ReadLine({delimiter: '\n'});
-
+const parser = new ReadlineParser({delimiter: '\n'});
 serial.pipe(parser)
 
-// Continously read in Data
+
+// Check if SerialPort was successfully opened
+serial.on('open', () =>
+{
+    console.log("Serial port is open.");
+});
+
+serial.on('error', (err) => 
+{
+    console.log(err.message);
+});
+
+
+// Continously read in data
 parser.on('data', (line) =>
 {
+    
     if (recording)
     {
+        console.log("Started logging.");
         const inputClean = line.trim();
         const inputArray = inputClean.split(" ");
         const timestamp = parseInt(inputArray[0]);
@@ -40,29 +60,50 @@ parser.on('data', (line) =>
 });
 
 
-// Starts Server
-app.listen(port, () =>
-{
-    console.log("Express is on at port: ", port);
-});
-
-
 
 // Start Recording
-app.post("/start", (request, response))
+app.post('/start', (request, response) =>
 {
+    
     emgData = [];
     recording = true;
 
     console.log("Started recording.");
     response.sendStatus(200);
-}
+})
 
 // Stop Recording
-app.post("/stop", (request, response))
+app.post('/stop', (request, response) =>
 {
     recording = false;
 
     console.log("Stopped Recording.");
     response.json(emgData);
-}
+})
+
+app.post('/callGemini', async (request, response)  =>
+{
+    // Follow Gemini docs to get a AI response and then return it
+    const aiAnswer = await ai.models.generateContent(
+    {
+        model: "gemini-2.5-flash",
+        contents: ` I am a 20 year old who exercises 4-5x a week. I eat a strict diet, no sugar and only organic food.
+                    These are my emg results after placing on my forearm and squeezing a few times for a few seconds.
+                    The data is has the time in ms with the first value being the starting point.
+                    The values are in mV. Please analyze this data and report an accurate response.` + JSON.stringify(emgData), 
+        config: {
+          thinkingConfig: {
+            thinkingBudget: 0, // Disables thinking
+          },
+        }
+      });
+    
+    response.send(aiAnswer.text);
+})
+
+
+// Starts Server at port 3001
+app.listen(port, () =>
+{
+    console.log("Express is on at port: ", port);
+});
